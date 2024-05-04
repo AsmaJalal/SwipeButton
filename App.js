eb39534635f1a7aff072cab1bdfff24efaea1529
Asma Jalal <asmaarain098@gmail.com>
2024-05-04 15:30:35 +0500

    customcomponents

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+import SwipeButton from './src/Swipe/components/SwipeButton';
+import AnimatedAccordion from './src/Swipe/components/AnimatedAccordion';
+import AnimatedSwitchButton from './src/Swipe/components/AnimatedSwitchButton';
+import SwitchListItems from './src/Swipe/toggleSwitchList/toggleSwitchList';
+import FlaotingTextInput from './src/Swipe/components/FloatingTextInput'
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <SwipeButton onToggle={() => {}}/>
+      <AnimatedAccordion />
+      <AnimatedSwitchButton />
+      <SwitchListItems/>
+      <FlaotingTextInput />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'#95C8D8',
+  },
+});
+
+export default App;
